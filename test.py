0c71d76ea0890ab39ae8e7056ece2f7a850ae666
theRealProHacker <rosk.abed@gmail.com>
2025-10-08 16:56:45 +0200

    Human tests

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import asyncio
-from app import clean_chat_history, log_chat, make_key_chat_history
+
+from app import clean_chat_history, gen_key, log_chat, make_key_chat_history, supabase
 
 async def test_1():
     await log_chat([
@@ -19,8 +20,8 @@ async def test_1():
 async def test():
     """Gets two chats and compares them"""
 
-    chat1_id = "512b9499-37cb-43c2-af3e-059e099ae010"
-    chat2_id = "67bb560e-c118-4ed2-b56d-5b531df53b79"
+    chat1_id = "935294cf-d555-4f33-9446-468898e79ae6"
+    chat2_id = "2ae34133-0197-45e6-9c3c-90efa61740b8"
 
     # load chats from supabase
     from app import supabase
@@ -36,15 +37,30 @@ async def test():
     # for msg in chat2:
     #     print(msg)
 
-    if clean_chat_history(chat1) != clean_chat_history(make_key_chat_history(chat2)):
+    chat1 = clean_chat_history(make_key_chat_history(chat1))
+    chat2 = clean_chat_history(chat2)
+
+    print("Chat 1 length:", len(chat1))
+    print("Chat 2 length:", len(chat2))
+
+    if chat1 != chat2:
+        assert gen_key(chat1) != gen_key(chat2)
         print("Chats do not match")
 
         # which message is different?
         for i, (msg1, msg2) in enumerate(zip(chat1, chat2)):
             if msg1 != msg2:
                 print(f"Message {i} is different:")
-                print(f"Chat 1: {msg1}")
-                print(f"Chat 2: {msg2}")
+                print(f"Chat 1 (cleaned): {msg1}")
+                print(f"Chat 2 (cleaned): {msg2}")
+    else:
+        assert gen_key(chat1) == gen_key(chat2)
+        print("Chats match")
 
 
-asyncio.run(test())
+# asyncio.run(test())
+
+# Get the number of chats in september 2025
+
+chat_number = supabase.table("chats").select("id", count="exact").gte("timestamp", "2025-09-01T00:00:00Z").lt("timestamp", "2025-10-01T00:00:00Z").execute()
+print("Number of chats in September 2025:", chat_number.count)
